ee62e879768a54498289a2ac77c0fa2231933471
demola234 <kolawoleoluwasegun567@gmail.com>
2024-07-19 14:37:24 +0100

    Initial Commit

---
+++ CleanArchWeatherApp/CleanArchWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchWeatherAppApp.swift
+//  CleanArchWeatherApp
+//
+//  Created by Ademola Kolawole on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArchWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanArchWeatherApp
+//
+//  Created by Ademola Kolawole on 19/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
